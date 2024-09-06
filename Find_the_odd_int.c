cbf79907f85fc3586ae3cf41793f7af34ec28f9b
over-Y <1034775920@qq.com>
2024-09-06 12:06:24 +0800

    master

---
+++ Find_the_odd_int.c
@@ -0,0 +1,43 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+#include <stdio.h>
+#include <stddef.h>
+
+int find_odd (size_t length, const int array[length])
+{
+  
+  for(size_t i = 0; i < length; i++){
+    int count = 0;
+    int temp = array[i];
+    for(size_t j = 0; j < length; j++){
+      
+      if(array[j]==temp)
+        count++;
+    }
+    if(count % 2 == 1)
+      return temp;
+    
+  }
+    return 0;
+}
+
+
+
+// 测试
+int main(){
+    int arr[] = {1,2,2,3,3,3,4,3,3,3,2,2,1};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int result = find_odd(len, arr);
+    printf("%d",result);
+
+    return 0;
+}
